731b5f2b2d064fb54b2a56b7a24f42bd1a586efe
patrickAdegbesan <adegbesanpatrick14@gmail.com>
2025-09-29 09:44:13 +0100

    Fixed inventory system paths and routing

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
+const fs = require('fs');
 
 const { sequelize, testConnection } = require('./config/database');
 const { User, Location } = require('./models');
